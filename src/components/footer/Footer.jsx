aca61f7508c7981ce037fe494a2344e06ff4f073
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-05-20 19:52:56 +0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,20 +5,13 @@ function Footer() {
   const date = new Date();
 
   return (
-    // <div className='w-full py-4 bg-white drop-shadow text-center'>
-    //     copyright &copy; 2024
-    // </div>
+    
     <footer className="bg-white drop-shadow text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg> */}
-          {/* <span className="ml-3 text-xl">Tailblocks</span> */}
           <span className='ml-3 text-xl primary-color slide-left-anime ' style={{transition:'all 2s linear'}}>EasyMeet</span>
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">&copy; { date.getFullYear() } mughaltechlab 
-          {/* <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@knyttneve</a> */}
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a className="text-gray-500">
